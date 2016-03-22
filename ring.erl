412f97d60d4f242b45cf00d057bd6b637cfc95a1
ruofei xu <xu.ruofei@hotmail.com>
2016-03-22 09:57:02 -0700

    update

--- ring.erl
+++ ring.erl
@@ -1,10 +1,10 @@
 -module(ring).
--export([batch_init/1,forward/1,forward/2,ring1/2]).
+-export([batch_init/2,forward/1,forward/3,ring1/2]).
 
 % 1) the first node can send a
 % message and wait for it to arrive before sending the next message
 ring1(Processes, Msgs) ->
-  Header = spawn(ring,batch_init, [Processes]),
+  Header = spawn(ring,batch_init, [Processes,self()]),
   Header ! {1,Msgs}.
 
 % 2) the first node can send 
@@ -13,13 +13,13 @@ ring1(Processes, Msgs) ->
 
 forward(Dest) ->
   io:format("Process ~p sends to ~p~n", [self(),Dest]),
-  forwarder(Dest,-1).
+  forwarder(Dest,-1,-1).
 
-forward(Dest,N) ->
+forward(Dest,N,Main) ->
   io:format("Process ~p sends to ~p~n", [self(),Dest]),
-  forwarder(Dest,N).
+  forwarder(Dest,N,Main).
 
-forwarder(Dest, N) ->
+forwarder(Dest, N,Main) ->
   receive
     quit  ->
       Dest ! quit;
@@ -27,22 +27,23 @@ forwarder(Dest, N) ->
       if
         N < 0 ->
           io:format("Send ~p from ~p to ~p~n", [M, self(),Dest]),
-          Dest ! {M,Counter+1}, forward(Dest,N);
+          Dest ! {M,Counter+1}, forward(Dest,N,Main);
         Counter < N  ->
           io:format("Send ~p from ~p to ~p~n", [M, self(),Dest]),
-          Dest ! {M,Counter+1}, forward(Dest,N);
+          Dest ! {M,Counter+1}, forward(Dest,N,Main);
         true ->
           io:format("Stop sending"),
-          forward(Dest,N)
+          Main ! done,
+          forward(Dest,N,Main)
       end
   end.
 
-batch_init(N) ->
-  batch_init_loop(N, self()).
+batch_init(N,Main) ->
+  batch_init_loop(N, self(),Main).
 
-batch_init_loop(N, First) ->
+batch_init_loop(N, First,Main) ->
   Last = lists:foldl(fun spawnForwarder/2, First, lists:seq(1,N)),
-  forward(Last,N).
+  forward(Last,N,Main).
 
 spawnForwarder(_,A) ->
   spawn(ring, forward, [A]).
